9a5431cffae10c597a51a6ac572a98294267ec6c
Cory Dransfeldt <hi@coryd.dev>
2023-03-13 19:04:31 -0700

    tweak now timing

--- src/_data/now.js
+++ src/_data/now.js
@@ -3,7 +3,7 @@ const EleventyFetch = require('@11ty/eleventy-fetch')
 module.exports = async function () {
     const url = 'https://utils.coryd.dev/api/now?endpoints=artists,albums,books,movies,tv'
     const res = EleventyFetch(url, {
-        duration: '3h',
+        duration: '1h',
         type: 'json',
     })
     const now = await res
