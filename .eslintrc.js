ebc1f436103a08058dd7c11958bfb2e94b57c9dc
Cory Dransfeldt <hi@coryd.dev>
2023-03-25 12:07:41 -0700

    chore: drop create script

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,13 +14,9 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:jsx-a11y/recommended',
         'plugin:prettier/recommended',
-        'next',
-        'next/core-web-vitals',
     ],
     rules: {
         'prettier/prettier': 'error',
-        'react/react-in-jsx-scope': 'off',
-        'react-hooks/exhaustive-deps': 'warn',
         'jsx-a11y/anchor-is-valid': [
             'error',
             {
@@ -29,9 +25,7 @@ module.exports = {
                 aspects: ['invalidHref', 'preferButton'],
             },
         ],
-        'react/prop-types': 0,
         'no-unused-vars': 0,
-        'react/no-unescaped-entities': 0,
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
