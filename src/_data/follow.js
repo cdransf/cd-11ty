02e444e25ec5940f54df8f1d455e00273ead95ec
Cory Dransfeldt <hi@coryd.dev>
2023-03-28 10:53:42 -0700

    feat: json follow feed + github actions to post

---
+++ src/_data/follow.js
@@ -0,0 +1,13 @@
+module.exports = async function () {
+  const { ActivityFeed } = await import('@11ty/eleventy-activity-feed')
+  const feed = new ActivityFeed()
+  feed.addSource('atom', 'Blog', 'https://coryd.dev/feed.xml')
+  feed.addSource('rss', 'Letterboxd', 'https://letterboxd.com/cdme/rss')
+  feed.addSource('rss', 'Glass', 'https://glass.photo/coryd/rss')
+  feed.addSource('rss', 'Oku', 'https://oku.club/rss/collection/NvEmF')
+  const entries = feed.getEntries()
+  const res = await entries
+  const activity = { posts: [] }
+  res.forEach((entry) => activity.posts.push({ title: entry.title, url: entry.url }))
+  return activity
+}
