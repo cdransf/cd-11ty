c95317a2dffa442410f097d77ae752438ef9fcaf
Cory Dransfeldt <hi@coryd.dev>
2023-04-18 13:45:23 -0700

    fix: merge conflict

--- src/_data/albums.js
+++ src/_data/albums.js
@@ -2,7 +2,6 @@ const EleventyFetch = require('@11ty/eleventy-fetch')
 
 module.exports = async function () {
   const MUSIC_KEY = process.env.API_KEY_LASTFM
-  if (!MUSIC_KEY) return
   const url = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=cdme_&api_key=${MUSIC_KEY}&limit=8&format=json&period=7day`
   const res = EleventyFetch(url, {
     duration: '1h',
