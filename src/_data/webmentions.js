62b2518590fdaba0e657956bbb89213340091a3d
Cory Dransfeldt <hi@coryd.dev>
2023-04-18 13:15:00 -0700

    feat: integrate tinacms

--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -2,6 +2,7 @@ const EleventyFetch = require('@11ty/eleventy-fetch')
 
 module.exports = async function () {
   const KEY_CORYD = process.env.API_KEY_WEBMENTIONS_CORYD_DEV
+  if (!KEY_CORYD) return
   const url = `https://webmention.io/api/mentions.jf2?token=${KEY_CORYD}&per-page=1000`
   const res = EleventyFetch(url, {
     duration: '1h',
