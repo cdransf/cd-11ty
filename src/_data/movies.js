54e5e67643cd14ad8ed05d1e2e1252da5c8c4be3
Cory Dransfeldt <hi@coryd.dev>
2023-04-26 13:42:45 -0700

    feat: add movie posters to now page

--- src/_data/movies.js
+++ src/_data/movies.js
@@ -5,10 +5,17 @@ module.exports = async function () {
   const url = 'https://letterboxd.com/cdme/rss'
   const asset = new AssetCache('movies_data')
   if (asset.isCacheValid('1h')) return await asset.getCachedValue()
-  const res = await extract(url).catch((error) => {
+  const res = await extract(url, {
+    getExtraEntryFields: (feedEntry) => {
+      const images = feedEntry['description']?.match(/<img [^>]*src="[^"]*"[^>]*>/gm) || []
+      return {
+        image: images.length ? images.map((x) => x.replace(/.*src="([^"]*)".*/, '$1'))[0] : '',
+      }
+    },
+  }).catch((error) => {
     console.log(error.message)
   })
-  const data = res.entries.splice(0, 5)
+  const data = res.entries.splice(0, 6)
   await asset.save(data, 'json')
   return data
 }
