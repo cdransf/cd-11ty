ea01261952b726c9a8435bbe511a319887091e95
Cory Dransfeldt <hi@coryd.dev>
2023-05-05 16:28:07 -0700

    fix: feed data

--- src/_data/follow.js
+++ src/_data/follow.js
@@ -2,6 +2,7 @@ module.exports = async function () {
   const { ActivityFeed } = await import('@11ty/eleventy-activity-feed')
   const feed = new ActivityFeed()
   feed.addSource('atom', 'Blog', 'https://coryd.dev/feed.xml')
+  feed.addSource('atom', 'Link', 'https://coryd.dev/links.xml')
   feed.addSource('rss', 'Letterboxd', 'https://letterboxd.com/cdme/rss')
   feed.addSource('rss', 'Oku', 'https://oku.club/rss/collection/NvEmF')
   const entries = feed.getEntries()
