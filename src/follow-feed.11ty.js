43b7ce6bb16112bd3e12389d171cd099f70d398b
Cory Dransfeldt <hi@coryd.dev>
2023-05-07 13:23:00 -0700

    chore: follow feed formatting

--- src/follow-feed.11ty.js
+++ src/follow-feed.11ty.js
@@ -9,10 +9,10 @@ module.exports = class {
     const { ActivityFeed } = await import('@11ty/eleventy-activity-feed')
     const feed = new ActivityFeed()
 
-    feed.addSource('atom', 'Blog', 'https://coryd.dev/feed.xml')
-    feed.addSource('atom', 'Link', 'https://coryd.dev/links.xml')
-    feed.addSource('rss', 'Letterboxd', 'https://letterboxd.com/cdme/rss')
-    feed.addSource('rss', 'Oku', 'https://oku.club/rss/collection/NvEmF')
+    feed.addSource('atom', '📝', 'https://coryd.dev/feed.xml')
+    feed.addSource('atom', '🔗', 'https://coryd.dev/links.xml')
+    feed.addSource('rss', '🎥', 'https://letterboxd.com/cdme/rss')
+    feed.addSource('rss', '📖', 'https://oku.club/rss/collection/NvEmF')
 
     return feed.toRssFeed({
       title: "Cory Dransfeldt's activity feed",
