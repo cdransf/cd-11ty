6f68d5a9892f687c48759958adb152047b6d92fc
Cory Dransfeldt <hi@coryd.dev>
2023-05-27 09:58:09 -0700

    chore: use optimized images

--- config/mediaFilters.js
+++ config/mediaFilters.js
@@ -2,7 +2,7 @@ const ALBUM_DENYLIST = ['no-love-deep-web', 'unremittance']
 
 module.exports = {
   artist: (media) =>
-    `https://cdn.coryd.dev/artists/${media.replace(/\s+/g, '-').toLowerCase()}.jpg`,
+    `https://cdn.coryd.dev/artists/min/${media.replace(/\s+/g, '-').toLowerCase()}.webp`,
   album: (media) => {
     return !ALBUM_DENYLIST.includes(media.name.replace(/\s+/g, '-').toLowerCase())
       ? media.image[media.image.length - 1]['#text'].replace(
