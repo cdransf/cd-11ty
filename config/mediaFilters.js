de5cb2db8b5046b8676b1999254359a725fb3d56
Cory Dransfeldt <hi@coryd.dev>
2023-05-26 17:09:02 -0700

    chore: perf improvements

--- config/mediaFilters.js
+++ config/mediaFilters.js
@@ -5,7 +5,13 @@ module.exports = {
     `https://cdn.coryd.dev/artists/${media.replace(/\s+/g, '-').toLowerCase()}.jpg`,
   album: (media) => {
     return !ALBUM_DENYLIST.includes(media.name.replace(/\s+/g, '-').toLowerCase())
-      ? media.image[media.image.length - 1]['#text']
-      : `https://cdn.coryd.dev/artists/${media.name.replace(/\s+/g, '-').toLowerCase()}.jpg`
+      ? media.image[media.image.length - 1]['#text'].replace(
+          'https://lastfm.freetls.fastly.net',
+          'https://albums.coryd.dev'
+        )
+      : `https://cdn.coryd.dev/albums/${media.name.replace(/\s+/g, '-').toLowerCase()}.jpg`
+  },
+  proxy: (url, host, cdn) => {
+    return url.replace(host, cdn)
   },
 }
--- src/_data/tv.js
+++ src/_data/tv.js
@@ -7,10 +7,17 @@ module.exports = async function () {
   // noinspection JSCheckFunctionSignatures
   const asset = new AssetCache('tv_data')
   if (asset.isCacheValid('1h')) return await asset.getCachedValue()
-  const res = await extract(url).catch((error) => {
+  const res = await extract(url, {
+    getExtraEntryFields: (feedEntry) => {
+      const image = feedEntry['media:thumbnail']['@_url']
+      return {
+        image,
+      }
+    },
+  }).catch((error) => {
     console.log(error.message)
   })
-  const data = res.entries.splice(0, 5)
+  const data = res.entries.splice(0, 6)
   await asset.save(data, 'json')
   return data
 }
