e46de0f1c1a2881cc586ab5405e26f8c618824f6
Cory Dransfeldt <hi@coryd.dev>
2023-06-11 09:35:53 -0700

    fix: handle tv shows that include :

--- config/mediaFilters.js
+++ config/mediaFilters.js
@@ -11,7 +11,8 @@ module.exports = {
         )
       : `https://cdn.coryd.dev/albums/${media.name.replace(/\s+/g, '-').toLowerCase()}.jpg`
   },
-  tv: (episode) => `https://cdn.coryd.dev/tv/${episode.replace(/\s+/g, '-').toLowerCase()}.jpg`,
+  tv: (episode) =>
+    `https://cdn.coryd.dev/tv/${episode.replace(':', '').replace(/\s+/g, '-').toLowerCase()}.jpg`,
   cdn: (url, host, cdn) => {
     return url.replace(host, cdn)
   },
