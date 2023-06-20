8bb5361403cbda0c79d5289b4b87cf372650ec24
Cory Dransfeldt <hi@coryd.dev>
2023-06-20 10:30:51 -0700

    chore: more reorganization

--- .eleventy.js
+++ .eleventy.js
@@ -43,8 +43,6 @@ module.exports = function (eleventyConfig) {
 
   // passthrough
   eleventyConfig.addPassthroughCopy('src/assets')
-  eleventyConfig.addPassthroughCopy('src/robots.txt')
-  eleventyConfig.addPassthroughCopy('src/contribute.json')
   eleventyConfig.addPassthroughCopy('_redirects')
 
   // shortcodes
