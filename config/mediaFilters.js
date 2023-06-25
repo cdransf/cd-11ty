7c3e71c0c9d4390b4f9a697b222e0ae636f12315
Cory Dransfeldt <hi@coryd.dev>
2023-06-25 12:33:31 -0700

    chore: additional fallback images

--- config/mediaFilters.js
+++ config/mediaFilters.js
@@ -1,8 +1,10 @@
 module.exports = {
   artist: (media) =>
-    `https://cdn.coryd.dev/artists/${media.replace(/\s+/g, '-').toLowerCase()}.jpg`,
+    `https://cdn.coryd.dev/artists/${media.replace(/\s+/g, '-').toLowerCase()}.jpg` ||
+    'https://cdn.coryd.dev/artists/missing-artist.jpg',
   tv: (episode) =>
-    `https://cdn.coryd.dev/tv/${episode.replace(':', '').replace(/\s+/g, '-').toLowerCase()}.jpg`,
+    `https://cdn.coryd.dev/tv/${episode.replace(':', '').replace(/\s+/g, '-').toLowerCase()}.jpg` ||
+    'https://cdn.coryd.dev/tv/missing-tv.jpg',
   cdn: (url, host, cdn) => {
     return url.replace(host, cdn)
   },
