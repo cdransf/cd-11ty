33849fafe136d72e49818efc5c4faf799b3417a8
Cory Dransfeldt <hi@coryd.dev>
2023-06-26 20:54:59 -0700

    chore: add clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
     require('@catppuccin/tailwindcss', {
       defaultFlavour: 'mocha',
     }),
