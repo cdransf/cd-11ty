b7a955a3ab28a3300f5ffa78dc20fcea11503194
Cory Dransfeldt <hi@coryd.dev>
2023-07-08 20:06:53 -0700

    fix: book data

--- src/_data/books.js
+++ src/_data/books.js
@@ -1,14 +1,15 @@
 const reading = require('./json/read.json')
 
 module.exports = async function () {
-  return reading.map(read => {
-    if (read.status === 'started') return {
+  const books = reading.map(read => {
+    return {
       title: read.title,
-      author: read.authors.length > 1 ? read.authors.join(', ') : read.authors.pop(),
       cover: read.thumbnail.replace('https://books.google.com', 'https://books.coryd.dev'),
       link: `https://openlibrary.org/search?q=${read.isbn}`,
       started: read.dateStarted,
-      finished: read.dateFinished
+      finished: read.dateFinished,
+      status: read.status,
     }
   })
+  return books.filter(book => book.status === 'started')
 }
