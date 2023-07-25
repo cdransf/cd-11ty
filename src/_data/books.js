af8ac970c515e012938dac0e3610446382ed1669
Cory Dransfeldt <hi@coryd.dev>
2023-07-25 16:55:30 -0700

    chore: update book access patterns

--- src/_data/books.js
+++ src/_data/books.js
@@ -1,15 +1,17 @@
 const reading = require('./json/read.json')
 
 module.exports = async function () {
-  const books = reading.map(read => {
+  const books = reading.map((read) => {
     return {
-      title: read.title,
-      cover: read.thumbnail.replace('https://books.google.com', 'https://books.coryd.dev').replace('&edge=curl',''),
-      link: `https://openlibrary.org/search?q=${read.isbn}`,
-      started: read.dateStarted,
-      finished: read.dateFinished,
-      status: read.status,
+      title: read['title'],
+      cover: read['thumbnail']
+        .replace('https://books.google.com', 'https://books.coryd.dev')
+        .replace('&edge=curl', ''),
+      link: `https://openlibrary.org/search?q=${read['isbn']}`,
+      started: read['dateStarted'],
+      finished: read['dateFinished'],
+      status: read['status'],
     }
   })
-  return books.filter(book => book.status === 'started')
+  return books.filter((book) => book.status === 'started')
 }
