8151b336339578bfbab083571f5b4b388deae42a
Cory Dransfeldt <coryd@hey.com>
2023-09-04 14:50:39 -0700

    chore: temp fix

--- src/_data/follow.js
+++ src/_data/follow.js
@@ -1,10 +1,7 @@
 module.exports = async function () {
   const { ActivityFeed } = await import('@11ty/eleventy-activity-feed')
   const feed = new ActivityFeed()
-  feed.addSource('atom', '📝', 'https://coryd.dev/feeds/posts')
-  feed.addSource('atom', '🔗', 'https://coryd.dev/feeds/links')
-  feed.addSource('rss', '🎥', 'https://letterboxd.com/cdme/rss')
-  feed.addSource('atom', '📖', 'https://coryd.dev/feeds/books')
+  
   const entries = feed.getEntries().catch()
   const res = await entries
   const activity = { posts: [] }
