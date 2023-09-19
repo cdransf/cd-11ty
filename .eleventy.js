55d74b634572b6b01ef27eb063860f451395075c
Cory Dransfeldt <cory.dransfeldt@gmail.com>
2023-09-19 11:24:03 -0700

    chore: feed updates

--- .eleventy.js
+++ .eleventy.js
@@ -91,7 +91,7 @@ module.exports = function (eleventyConfig) {
               url: post.data.link,
               title: post.data.title,
               date: post.data.date,
-              description: post.data.post_excerpt,
+              description: md.render(post.data.post_excerpt),
             })
         })
       }
