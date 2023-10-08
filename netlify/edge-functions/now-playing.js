84077283b4688e8e3836a738bb0c1088fe5ced6e
Cory Dransfeldt <coryd@fastmail.com>
2023-10-07 17:20:26 -0700

    feat: show tv or movie if checked in, music if not

--- netlify/edge-functions/now-playing.js
+++ netlify/edge-functions/now-playing.js
@@ -3,6 +3,39 @@ export default async () => {
   const API_APPLE_MUSIC_DEVELOPER_TOKEN = Netlify.env.get('API_APPLE_MUSIC_DEVELOPER_TOKEN')
   // eslint-disable-next-line no-undef
   const API_APPLE_MUSIC_USER_TOKEN = Netlify.env.get('API_APPLE_MUSIC_USER_TOKEN')
+  // eslint-disable-next-line no-undef
+  const TV_KEY = Netlify.env.get('API_KEY_TRAKT')
+
+  const traktRes = await fetch('https://api.trakt.tv/users/cdransf/watching', {
+    headers: {
+      'Content-Type': 'application/json',
+      'trakt-api-version': 2,
+      'trakt-api-key': TV_KEY,
+    },
+  })
+    .then((data) => {
+      if (data.body) return data.json()
+      return {}
+    })
+    .catch()
+
+  if (Object.keys(traktRes).length) {
+    if (traktRes['type'] === 'episode') {
+      return Response.json({
+        title: traktRes['show']['title'],
+        episode: traktRes['episode']['title'],
+        text: `📺 ${traktRes['show']['title']}: ${traktRes['episode']['title']}`,
+      })
+    }
+
+    if (traktRes['type'] === 'movie') {
+      return Response.json({
+        title: traktRes['movie']['title'],
+        text: `🎥 ${traktRes['movie']['title']}`,
+      })
+    }
+  }
+
   const trackRes = await fetch('https://api.music.apple.com/v1/me/recent/played/tracks?limit=1', {
     headers: {
       'Content-Type': 'application/json',
