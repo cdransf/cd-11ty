5399324bbaa3bbbd7b798f7bc5d9d36a465f424d
Cory Dransfeldt <coryd@fastmail.com>
2023-10-21 14:04:50 -0700

    fix: map + subtext

--- src/utils/media.js
+++ src/utils/media.js
@@ -37,7 +37,11 @@ module.exports = {
    * @param {Array} array - an array of media objects.
    * @returns {Array} - a new array sorted by the number of plays in descending order.
    */
-  sortByPlays: (array) => Object.values(array).sort((a, b) => b.plays - a.plays).map((media, index) => {
-    return media.rank = index + 1
-  }),
+  sortByPlays: (array) =>
+    Object.values(array)
+      .sort((a, b) => b.plays - a.plays)
+      .map((media, index) => {
+        media.rank = index + 1
+        return media
+      }),
 }
