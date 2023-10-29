5bd5aede2296dc20be5229a11f9f309151e4325a
Cory Dransfeldt <coryd@fastmail.com>
2023-10-28 19:55:48 -0700

    chore: extract read percentage

--- src/_data/books.js
+++ src/_data/books.js
@@ -3,11 +3,16 @@ const { AssetCache } = require('@11ty/eleventy-fetch')
 const { JSDOM } = jsdom
 
 module.exports = async function () {
+  const COOKIE = process.env.COOKIE_STORYGRAPH
   const url = 'https://app.thestorygraph.com/currently-reading/coryd'
   const asset = new AssetCache('books_data')
   if (asset.isCacheValid('1h')) return await asset.getCachedValue()
   const data = []
-  await fetch(url)
+  await fetch(url, {
+    headers: {
+      Cookie: COOKIE,
+    },
+  })
     .then((res) => res.text())
     .then((html) => {
       const DOM = new JSDOM(html)
@@ -33,6 +38,12 @@ module.exports = async function () {
         if (!data[index]) data.push({ url: `https://app.thestorygraph.com${url.href}` })
         if (data[index]) data[index]['url'] = `https://app.thestorygraph.com${url.href}`
       })
+      doc
+        .querySelectorAll('.md\\:block .progress-tracker-pane .font-semibold')
+        .forEach((percentage, index) => {
+          if (!data[index]) data.push({ percentage: percentage.textContent })
+          if (data[index]) data[index]['percentage'] = percentage.textContent
+        })
     })
   const books = data
     .filter((book) => book.title)
