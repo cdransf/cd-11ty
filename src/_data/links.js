1927790d06d0073eefdc05305f4eb8c2e5c9f566
Cory Dransfeldt <hi@coryd.dev>
2023-11-08 11:48:38 -0800

    chore: resolve conflict

--- src/_data/links.js
+++ src/_data/links.js
@@ -1,20 +1,5 @@
-const EleventyFetch = require('@11ty/eleventy-fetch')
+const links = require('../_data/json/links.json')
 
 module.exports = async function () {
-  const API_TOKEN_PINBOARD = process.env.API_TOKEN_PINBOARD
-  const url = `https://api.pinboard.in/v1/posts/recent?auth_token=${API_TOKEN_PINBOARD}&format=json&tag=share`
-  const res = EleventyFetch(url, {
-    duration: '1h',
-    type: 'json',
-  }).catch()
-  const links = await res
-  return links['posts'].map((link) => {
-    return {
-      title: link['description'],
-      url: link['href'],
-      tags: [...new Set(link['tags'].split(' ').filter((tag) => tag.toLowerCase() !== 'share'))],
-      date: link['time'],
-      description: link['extended'],
-    }
-  })
+  return links.links.reverse()
 }
