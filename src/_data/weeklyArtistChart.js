87b3cf2ad1c5c7037d098180860b60b200f2059e
Cory Dransfeldt <hi@coryd.dev>
2023-11-14 16:20:53 -0800

    chore: tweak output

--- src/_data/weeklyArtistChart.js
+++ src/_data/weeklyArtistChart.js
@@ -3,11 +3,14 @@ const chartData = require('./json/weekly-artist-charts.json')
 module.exports = async function () {
   const artists = chartData['weeklyartistchart']['artist'].splice(0, 8)
   const date = parseInt(chartData['weeklyartistchart']['@attr']['to']) * 1000
-  let content = 'My top artists for the week:\n'
-  artists.forEach((artist) => {
-    content += `#${artist['@attr']['rank']} ${artist['name']}: ${artist['playcount']} plays\n`
+  let content = 'My top artists for the week: '
+  artists.forEach((artist, index) => {
+    content += `${artist['name']} @ ${artist['playcount']} play${
+      parseInt(artist['playcount']) > 1 ? 's' : ''
+    }`
+    if (index !== artists.length - 1) content += ', '
   })
-  content += '#Music #LastFM'
+  content += ' #Music #LastFM'
   return [
     {
       title: content,
