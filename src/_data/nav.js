13ba6145c194387d0da25e48140ffffc2607e881
Cory Dransfeldt <cory.dransfeldt@gmail.com>
2023-11-28 11:47:28 -0800

    chore: nav updates

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -7,8 +7,6 @@ module.exports = async function () {
       { name: 'Tags', icon: 'tags' },
       { name: 'Search', icon: 'search' },
       { name: 'Feeds', icon: 'rss' },
-      { name: 'Mastodon', url: 'https://social.lol/@cory', icon: 'brand-mastodon' },
-      { name: 'Buy Me a Coffee', url: 'https://www.buymeacoffee.com/cory', icon: 'cup' },
     ],
     social: [
       { name: 'Email', url: 'mailto:cory.dransfeldt@gmail.com', icon: 'brand-gmail' },
@@ -18,7 +16,7 @@ module.exports = async function () {
       { name: 'Trakt', url: 'https://trakt.tv/users/cdransf', icon: 'device-tv' },
       { name: 'The StoryGraph', url: 'https://app.thestorygraph.com/profile/coryd', icon: 'books' },
       { name: 'Buy Me a Coffee', url: 'https://www.buymeacoffee.com/cory', icon: 'cup' },
-      { name: 'Webrings', icon: 'heart-handshake' },
+      { name: 'Webrings', url: '/webrings', icon: 'heart-handshake' },
     ],
   }
 }
