b8728e7169c05f1a65c1afda1f2988997aa7566b
Cory Dransfeldt <cory.dransfeldt@gmail.com>
2023-12-28 16:32:48 -0800

    chore: roundup post

---
+++ src/_data/json/roundups/2023.js
@@ -0,0 +1,98 @@
+export default {
+  books: [
+    {
+      alt: 'Stay True',
+      author: 'Hua Hsu',
+      image: 'https://cd-books.b-cdn.net/8jdewx4ipwb9hro2oedjkfj1cy0f',
+      url: 'https://app.thestorygraph.com/books/9946c834-81f1-4c7f-b7d1-30a804e9874f',
+    },
+    {
+      alt: 'Where Are Your Boys Tonigh?',
+      author: 'Chris Payne',
+      image: 'https://cd-books.b-cdn.net/vajp3jxy6kee5ka2ymbvjc2fqkvf',
+      url: 'https://app.thestorygraph.com/books/f074d4e3-a9fc-42af-889e-54697a1fece0',
+    },
+    {
+      title: 'Trouble Boys',
+      author: 'Bob Mehr',
+      image: 'https://cd-books.b-cdn.net/66eh71z4igv2dsinrk7mif50fa6y',
+      url: 'https://app.thestorygraph.com/books/8dce0e20-fef1-42a4-a59b-b4ec084dc6f4',
+    },
+    {
+      alt: 'Corporate Rock Sucks',
+      author: 'Jim Ruland',
+      image: 'https://cd-books.b-cdn.net/tzf2l7725ydzzvvmzpbky7wj7ckc',
+      url: 'https://app.thestorygraph.com/books/8a0b8649-8939-4753-8e8d-18500574614e',
+    },
+    {
+      alt: 'Tracers in the Dark',
+      author: 'Andy Greenberg',
+      image: 'https://cd-books.b-cdn.net/m4s6lp9eljzk5vjm1xauou8frxde',
+      url: 'https://app.thestorygraph.com/books/4f1f21f8-3d1f-4162-9f6c-5a00a33f629c',
+    },
+    {
+      alt: 'Girl in a Band',
+      author: 'Kim Gordon',
+      image: 'https://cd-books.b-cdn.net/qnxw68i4xn3byegvkzq6kty0rlx7',
+      url: 'https://app.thestorygraph.com/books/c0ea8ac6-d2c6-43b6-be16-ba793e71bfc2',
+    }
+  ],
+  albums: [
+    {
+      title: 'the whaler',
+      artist: 'home is where',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/1d9cf6fc699a8e1f0687c2db51a85619.jpg#1d9cf6fc699a8e1f0687c2db51a85619',
+      url: 'https://musicbrainz.org/release-group/6fe3516f-c324-4265-8f43-d902f3a4cc20',
+      type: 'album',
+    },
+    {
+      title: 'The Enduring Spirit',
+      artist: 'Tomb Mold',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/c104455917804d05acbda1190a5e4db4.jpg#c104455917804d05acbda1190a5e4db4',
+      url: 'https://musicbrainz.org/release-group/cd3e5dfb-acca-4856-80f6-2e095ac3270d',
+      type: 'album',
+    },
+    {
+      title: 'A Dialogue With The Eeriest Sublime',
+      artist: 'Vertebra Atlantis',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/17c1cf7deccceb28c5da47bfda996269.jpg#17c1cf7deccceb28c5da47bfda996269',
+      url: 'https://musicbrainz.org/release-group/b8f1913b-f461-443c-a26c-377b259f2af6',
+      type: 'album',
+    },
+    {
+      title: 'ONE MORE TIME...',
+      artist: 'blink-182',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/a5de6e89887258fb4accce4a314c12a1.jpg#a5de6e89887258fb4accce4a314c12a1',
+      url: 'https://musicbrainz.org/release-group/520d6d45-19c8-4ee1-a954-180e7902f3da',
+      type: 'album',
+    },
+    {
+      title: 'Life Like',
+      artist: 'Dead Bob',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/1054f8e7ea6b66e6127fac3d3a5e7a18.jpg#1054f8e7ea6b66e6127fac3d3a5e7a18',
+      url: 'https://musicbrainz.org/release-group/ab53e625-74af-4a09-a8ff-e1c08dbae596',
+      type: 'album',
+    },
+    {
+      title: 'Threads of Unknowing',
+      artist: 'VoidCeremony',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/9fde93dca475dfab877c06d8e7c300bd.jpg#9fde93dca475dfab877c06d8e7c300bd',
+      url: 'https://musicbrainz.org/release-group/f1f91cde-ff57-41c8-bd58-28c236b3f0c6',
+      type: 'album',
+    },
+    {
+      title: 'Why Would I Watch',
+      artist: 'Hot Mulligan',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/21e3053a3faec5dd7a5bf65f6305f345.jpg#21e3053a3faec5dd7a5bf65f6305f345',
+      url: 'https://musicbrainz.org/release-group/5afd31ea-3a96-4b99-a477-4d121efaedec',
+      type: 'album',
+    },
+    {
+      title: 'Losing What We Love',
+      artist: 'Knuckle Puck',
+      image: 'https://cd-albums.b-cdn.net/i/u/770x0/cbbbf886c2518f41a65140ebb8c880cd.jpg#cbbbf886c2518f41a65140ebb8c880cd',
+      url: 'https://musicbrainz.org/release-group/b51d8882-3854-400a-b79b-4353a77a389b',
+      type: 'album',
+    }
+  ]
+}
---
+++ src/_data/roundups.js
@@ -0,0 +1,7 @@
+import TwentyTwentyThree from './json/roundups/2023.js'
+
+export default async function () {
+  return {
+    2023: TwentyTwentyThree
+  }
+}
