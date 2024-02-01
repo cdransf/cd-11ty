3edecfe47c0e0618c271ff7073e1fdc611da0d81
Cory Dransfeldt <hi@coryd.dev>
2024-02-01 12:31:43 -0800

    chore: dry up robots.txt templating

---
+++ src/_data/json/robots.js
@@ -0,0 +1,17 @@
+export default [
+  'AdsBot-Google',
+  'Amazonbot',
+  'anthropic-ai',
+  'ArcMobile2',
+  'Bytespider',
+  'CCBot',
+  'ChatGPT-User',
+  'cohere-ai',
+  'FacebookBot',
+  'Google-Extended',
+  'GPTBot',
+  'omgili',
+  'Omgilibot',
+  'PerplexityBot',
+  'YouBot',
+]
---
+++ src/_data/robots.js
@@ -0,0 +1,5 @@
+import robots from './json/robots.js'
+
+export default async function () {
+  return robots
+}
