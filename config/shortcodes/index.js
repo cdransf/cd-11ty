c8aabb7c2a13976813859a82874164d73a421cae
Cory Dransfeldt <hi@coryd.dev>
2024-02-04 13:00:36 -0800

    chore: clean up

--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -1,15 +1,11 @@
-import Image from '@11ty/eleventy-img'
-import path from 'path'
-import htmlmin from 'html-minifier-terser'
+import Image from '@11ty/eleventy-img';
+import path from 'path';
+import htmlmin from 'html-minifier-terser';
 
-const stringifyAttributes = (attributeMap) => {
-  return Object.entries(attributeMap)
-    .map(([attribute, value]) => {
-      if (typeof value === 'undefined') return '';
-      return `${attribute}="${value}"`;
-    })
+const stringifyAttributes = (attributeMap) =>
+  Object.entries(attributeMap)
+    .map(([attribute, value]) => (value === undefined ? '' : `${attribute}="${value}"`))
     .join(' ');
-};
 
 export const img = async (
   src,
@@ -20,13 +16,18 @@ export const img = async (
   icon,
   maxWidth = 1248,
   sizes = '90vw',
-  formats = ['avif', 'webp', 'jpeg'],
+  formats = ['avif', 'webp', 'jpeg']
 ) => {
-  const isLocal = src.includes('src/assets')
+  const isLocal = src.includes('src/assets');
   const imageExists = async () => {
-    const isOk = await fetch(src, { method: 'HEAD' }).then(res => res.ok).catch(() => false)
-    return isOk
-  }
+    try {
+      const isOk = await fetch(src, { method: 'HEAD' }).then((res) => res.ok);
+      return isOk;
+    } catch {
+      return false;
+    }
+  };
+
   const generateImage = async () => {
     const widths = [320, 570, 880, 1024, 1248];
     const metadata = await Image(src, {
@@ -35,8 +36,7 @@ export const img = async (
       outputDir: './_site/assets/img/cache/',
       urlPath: '/assets/img/cache/',
       filenameFormat: (id, src, width, format) => {
-        const extension = path.extname(src);
-        const name = path.basename(src, extension);
+        const { ext, name } = path.parse(src);
         return `${name}-${width}w.${format}`;
       },
     });
@@ -44,13 +44,12 @@ export const img = async (
     const lowsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
     const imageSources = Object.values(metadata)
-      .map((imageFormat) => {
-        return `  <source type="${
-          imageFormat[0].sourceType
-        }" srcset="${imageFormat
-          .map((entry) => entry.srcset)
-          .join(', ')}" sizes="${sizes}">`;
-      })
+      .map(
+        (imageFormat) =>
+          `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat
+            .map((entry) => entry.srcset)
+            .join(', ')}" sizes="${sizes}">`
+      )
       .join('\n');
 
     const imgageAttributes = stringifyAttributes({
@@ -63,25 +62,15 @@ export const img = async (
       decoding: 'async',
     });
 
-    const imageElement = `<picture>
-      ${imageSources}
-      <img ${imgageAttributes} />
-    </picture>`;
+    const imageElement = `<picture>${imageSources}<img ${imgageAttributes} /></picture>`;
 
     return htmlmin.minify(imageElement, { collapseWhitespace: true });
-  }
-  const generatePlaceholder = async () => {
-    const placeholderElement = `<div class="flex--centered image__placeholder ${shape}">
-      ${icon}
-    </div>`
+  };
 
+  const generatePlaceholder = async () => {
+    const placeholderElement = `<div class="flex--centered image__placeholder ${shape}">${icon}</div>`;
     return htmlmin.minify(placeholderElement, { collapseWhitespace: true });
-  }
-  if (isLocal) return await generateImage();
-  if (!isLocal) {
-    return await imageExists().then(async exists => {
-      if (exists) return await generateImage();
-      if (!exists) return await generatePlaceholder();
-    })
-  }
+  };
+
+  return isLocal ? await generateImage() : await imageExists().then(async (exists) => (exists ? await generateImage() : await generatePlaceholder()));
 };
