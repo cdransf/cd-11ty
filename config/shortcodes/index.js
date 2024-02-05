84e5c844f5a036b77d6cefad7766dcf602fddbbe
Cory Dransfeldt <hi@coryd.dev>
2024-02-05 13:18:51 -0800

    Revert "feat: crop images before optimizing them to ensure proper aspect ratio"
    This reverts commit 605baf5836cba5e5b6dc5379b54a2b3035d48fab.

--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -1,59 +1,18 @@
 import Image from '@11ty/eleventy-img';
 import path from 'path';
-import Sharp from 'sharp';
 import htmlmin from 'html-minifier-terser';
 
-const stringifyAttributes = attributeMap =>
+const stringifyAttributes = (attributeMap) =>
   Object.entries(attributeMap)
-    .map(([attribute, value]) =>
-      value === undefined
-        ? ''
-        : `${attribute}="${typeof value === 'object' ? value.value : value}"`
-    )
+    .map(([attribute, value]) => (value === undefined ? '' : `${attribute}="${value}"`))
     .join(' ');
-
-const resizeImage = async (src, width, height, mode, shape) => {
-  const commonOptions = {
-    fit: mode,
-    position: 'center',
-    background: { r: 255, g: 255, b: 255, alpha: 1 },
-  };
-
-  if (shape === 'square') {
-    const buffer = await Sharp(src)
-      .resize({
-        width: Math.min(width, height),
-        height: Math.min(width, height),
-        ...commonOptions,
-      })
-      .toBuffer();
-
-    return { buffer, width: Math.min(width, height), height: Math.min(width, height) };
-  } else if (shape === 'vertical') {
-    const aspectRatio = 2 / 3;
-    const targetWidth = Math.min(Math.floor(height * aspectRatio), width);
-    const buffer = await Sharp(src)
-      .resize({
-        width: targetWidth,
-        height: Math.floor(targetWidth / aspectRatio),
-        ...commonOptions,
-      })
-      .toBuffer();
-
-    return { buffer, width: targetWidth, height: Math.floor(targetWidth / aspectRatio) };
-  } else {
-    const buffer = await Sharp(src).toBuffer();
-
-    return { buffer, width, height };
-  }
-};
 
 export const img = async (
   src,
   alt = '',
   className,
   loading = 'lazy',
-  shape = '',
+  shape = 'square',
   icon,
   maxWidth = 1248,
   sizes = '90vw',
@@ -62,11 +21,13 @@ export const img = async (
   const isLocal = src.includes('src/assets');
   const imageExists = async () => {
     try {
-      return await fetch(src, { method: 'HEAD' }).then(res => res.ok);
+      const isOk = await fetch(src, { method: 'HEAD' }).then((res) => res.ok);
+      return isOk;
     } catch {
       return false;
     }
   };
+
   const generateImage = async () => {
     const widths = [320, 570, 880, 1024, 1248];
     const metadata = await Image(src, {
@@ -79,58 +40,31 @@ export const img = async (
         return `${name}-${width}w.${format}`;
       },
     });
+
     const lowsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
-    if (shape === 'square' || shape === 'vertical') {
-      const { buffer, width: resizedWidth, height: resizedHeight } = await resizeImage(
-        `./_site/${lowsrc.url}`,
-        lowsrc.width,
-        lowsrc.height,
-        'cover',
-        shape
-      );
-      const resizedImageBase64 = buffer.toString('base64');
-      const resizedImageSrc = `data:image/${lowsrc.format};base64,${resizedImageBase64}`;
-      const imageSources = Object.values(metadata)
-        .map((imageFormat) =>
+    const imageSources = Object.values(metadata)
+      .map(
+        (imageFormat) =>
           `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat
             .map((entry) => entry.srcset)
             .join(', ')}" sizes="${sizes}">`
-        )
-        .join('\n');
-      const imageAttributes = stringifyAttributes({
-        src: resizedImageSrc,
-        width: resizedWidth,
-        height: resizedHeight,
-        alt,
-        class: className,
-        loading,
-        decoding: 'async',
-      });
-      const imageElement = `<picture>${imageSources}<img ${imageAttributes} /></picture>`;
+      )
+      .join('\n');
 
-      return htmlmin.minify(imageElement, { collapseWhitespace: true });
-    } else {
-      const imageSources = Object.values(metadata)
-        .map((imageFormat) =>
-          `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat
-            .map((entry) => entry.srcset)
-            .join(', ')}" sizes="${sizes}">`
-        )
-        .join('\n');
-      const imgageAttributes = stringifyAttributes({
-        src: lowsrc.url,
-        width: lowsrc.width,
-        height: lowsrc.height,
-        alt,
-        class: className,
-        loading,
-        decoding: 'async',
-      });
-      const imageElement = `<picture>${imageSources}<img ${imgageAttributes} /></picture>`;
+    const imgageAttributes = stringifyAttributes({
+      src: lowsrc.url,
+      width: lowsrc.width,
+      height: lowsrc.height,
+      alt,
+      class: className,
+      loading,
+      decoding: 'async',
+    });
 
-      return htmlmin.minify(imageElement, { collapseWhitespace: true });
-    }
+    const imageElement = `<picture>${imageSources}<img ${imgageAttributes} /></picture>`;
+
+    return htmlmin.minify(imageElement, { collapseWhitespace: true });
   };
 
   const generatePlaceholder = async () => {
@@ -138,5 +72,5 @@ export const img = async (
     return htmlmin.minify(placeholderElement, { collapseWhitespace: true });
   };
 
-  return isLocal ? await generateImage() : await imageExists().then(async exists => (exists ? await generateImage() : await generatePlaceholder()));
+  return isLocal ? await generateImage() : await imageExists().then(async (exists) => (exists ? await generateImage() : await generatePlaceholder()));
 };
