893d787b4ad6b8f460c4fb42063f2cf6f55be771
Cory Dransfeldt <hi@coryd.dev>
2024-02-07 15:47:50 -0800

    fix: mbid patch

--- src/_data/json/mbid-patches.js
+++ src/_data/json/mbid-patches.js
@@ -1,5 +1,6 @@
 export default {
   adramelech: '259e6968-a841-4fc5-b02b-54665b85977b',
+  'aesop rock': 'aba64937-3334-4c65-90a1-4e6b9d4d7ada',
   afi: '1c3919b2-43ca-4a4a-935d-9d50135ec0ef',
   augury: 'd651baba-5ce1-4315-b884-d951e0435418',
   autopsy: '1d097d38-d5ca-4cd4-9200-7f08eedd0875',
