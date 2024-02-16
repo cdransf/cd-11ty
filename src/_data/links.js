cfc9db4ef756cac3e23633724fada0234690ea9a
Cory Dransfeldt <coryd@hey.com>
2024-02-16 11:41:04 -0800

    feat: fetch a larger set of links

--- src/_data/links.js
+++ src/_data/links.js
@@ -2,7 +2,6 @@ import EleventyFetch from '@11ty/eleventy-fetch'
 
 export default async function () {
   const API_TOKEN_READWISE = process.env.API_TOKEN_READWISE
-  const url = 'https://readwise.io/api/v3/list?location=archive'
   const formatLinkData = (links) => links.map((link) => {
     return {
       title: link['title'],
@@ -15,17 +14,28 @@ export default async function () {
       description: `${link['summary']}<br/><br/>`,
     }
   })
+  const fullData = [];
+  let nextPageCursor = null;
+  let requestCount = 0;
 
-  const res = EleventyFetch(url, {
-    duration: '1h',
-    type: 'json',
-    fetchOptions: {
-      headers: {
-        Authorization: `Token ${API_TOKEN_READWISE}`,
+  while (true) {
+    const queryParams = new URLSearchParams()
+    if (nextPageCursor) queryParams.append('pageCursor', nextPageCursor)
+    const res = EleventyFetch(`https://readwise.io/api/v3/list?location=archive&${queryParams.toString()}`, {
+      duration: '1h',
+      type: 'json',
+      fetchOptions: {
+        headers: {
+          Authorization: `Token ${API_TOKEN_READWISE}`,
+        },
       },
-    },
-  }).catch()
-  const data = await res
-  return formatLinkData(data['results']).filter((link) => link.tags.includes('share'))
+    }).catch()
+    const data = await res
+    fullData.push(...data['results']);
+    nextPageCursor = data['nextPageCursor']
+    requestCount++;
+    if (!nextPageCursor || requestCount === 20) break;
+  }
 
+  return formatLinkData(fullData).filter((link) => link.tags.includes('share'))
 }
