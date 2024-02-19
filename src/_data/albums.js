7a15f47a8c9751acddd52e8231642638403bf115
Cory Dransfeldt <coryd@hey.com>
2024-02-19 14:29:45 -0800

    feat: more reliable images (hopefully)

--- src/_data/albums.js
+++ src/_data/albums.js
@@ -1,7 +1,11 @@
 import EleventyFetch from '@11ty/eleventy-fetch'
 
+const removeAccents = (inputStr) => {
+  const normalizedStr = inputStr.normalize('NFD');
+  return normalizedStr.replace(/[\u0300-\u036f]/g, '');
+};
+
 export default async function () {
-  const ALBUM_DENYLIST = ['no-love-deep-web', 'unremittance', 'celebratory-beheading']
   const MUSIC_KEY = process.env.API_KEY_LASTFM
   const url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=coryd_&api_key=${MUSIC_KEY}&limit=8&format=json&period=7day`
   const formatAlbumData = (albums) => albums.map((album) => {
@@ -10,12 +14,8 @@ export default async function () {
       artist: album['artist']['name'],
       plays: album['playcount'],
       rank: album['@attr']['rank'],
-      image: !ALBUM_DENYLIST.includes(album['name'].replace(/\s+/g, '-').toLowerCase())
-        ? album['image'][album['image'].length - 1]['#text'].replace(
-            'https://lastfm.freetls.fastly.net',
-            'https://cd-albums.b-cdn.net'
-          )
-        : `https://cdn.coryd.dev/albums/${album['name'].replace(/\s+/g, '-').toLowerCase()}.jpg`,
+      image: `https://cdn.coryd.dev/albums/${removeAccents(album['artist']['name']).replace(/\s+/g, '-').toLowerCase()}-${removeAccents(album['name'].replace(/[:\/\\,'']+/g
+, '').replace(/\s+/g, '-').toLowerCase())}.jpg`,
       url: album['mbid']
         ? `https://musicbrainz.org/album/${album['mbid']}`
         : `https://musicbrainz.org/taglookup/index?tag-lookup.artist=${album['artist'][
