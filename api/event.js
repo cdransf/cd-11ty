8f1f7b77e7bc3097452f35b72215ecadd3697dcf
Cory Dransfeldt <coryd@hey.com>
2024-03-24 09:50:55 -0700

    chore: debug

---
+++ api/event.js
@@ -0,0 +1,21 @@
+export default async (request, context) => {
+  fetch(`https://cdn.usefathom.com/?h=${encodeURIComponent(request['referer'] || 'unknown')}&sid=CWSVCDJC&cid=${context['requestId']}`)
+    .then((data) => {
+      console.log(data)
+      return {}
+    })
+    .catch(err => {
+      console.log(err);
+      return {}
+    });
+
+  return new Response(JSON.stringify({
+      status: 'success',
+    }),
+    { headers: { "Content-Type": "application/json" } }
+  )
+}
+
+export const config = {
+  path: "/api/event",
+};
