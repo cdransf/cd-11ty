642ab61886d025e8429e059a2c1d3bdd693b6534
Cory Dransfeldt <coryd@hey.com>
2024-03-24 12:47:36 -0700

    chore: fixes

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,13 +3,5 @@ let i;
 if (window.sessionStorage && !window.sessionStorage?.getItem('id')) sessionStorage.setItem('id', id)
 if (window.localStorage && window.localStorage.getItem('i')) i = localStorage.getItem('i')
 if (!window.fathom) {
-  fetch(`https://coryd.dev/api/event/?page=${encodeURIComponent(window.location.href)}&num=${sessionStorage.getItem('id' || id)}&lang=${encodeURIComponent(navigator.language)}&nav=${encodeURIComponent(navigator.userAgent)}&i=${i}`)
-    .then((data) => {
-      console.log(data)
-      return {}
-    })
-    .catch(err => {
-      console.log(err);
-      return {}
-    });
+  fetch(`https://coryd.dev/api/event/?page=${encodeURIComponent(window.location.href)}&num=${sessionStorage.getItem('id' || id)}&lang=${encodeURIComponent(navigator.language)}&nav=${encodeURIComponent(navigator.userAgent)}&i=${i}`).then((data) => {}).catch(err => {});
 }
