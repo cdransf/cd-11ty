d300f24e5fe82c8f0ea1bc6c4de9c3e1400e61cb
Cory Dransfeldt <coryd@hey.com>
2024-03-24 13:09:46 -0700

    fix: inits

--- api/event.js
+++ api/event.js
@@ -3,7 +3,6 @@ import { getStore } from '@netlify/blobs';
 
 export default async (request, context) => {
   const ns = new URL(request['url']).searchParams.get('ns')
-  const id = crypto.createHash('md5').update(`${context['ip']}${context['geo']['city']}`).digest('hex');
   const page = new URL(request['url']).searchParams.get('page')
   const num = new URL(request['url']).searchParams.get('num')
   const lang = decodeURIComponent(new URL(request['url']).searchParams.get('lang'))
@@ -16,10 +15,11 @@ export default async (request, context) => {
 
   if (ig) return;
   if (ns) {
+    const id = crypto.createHash('md5').update(`${context['ip']}${context['geo']['city']}`).digest('hex');
     const ids = getStore('ids')
     if (!ids.get(id)) ids.set(id, '')
-    const id = ids.get(id)
-    url = `https://cdn.usefathom.com/?h=${encodeURIComponent(page)}&sid=CWSVCDJC&cid=${id}`
+    const idVal = ids.get(id)
+    url = `https://cdn.usefathom.com/?h=${encodeURIComponent(page)}&sid=CWSVCDJC&cid=${idVal}`
   } else {
     url = `https://cdn.usefathom.com/?h=${encodeURIComponent(page)}&sid=CWSVCDJC&cid=${num}`
   }
