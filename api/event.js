a74878900aba4002ec0716533e957817625d7397
Cory Dransfeldt <coryd@hey.com>
2024-03-24 14:09:06 -0700

    chore: debug

--- api/event.js
+++ api/event.js
@@ -1,5 +1,5 @@
 import crypto from 'node:crypto'
-import { getStore, listStores } from '@netlify/blobs';
+import { getStore } from '@netlify/blobs';
 
 export default async (request, context) => {
   const ns = new URL(request['url']).searchParams.get('ns')
@@ -8,7 +8,6 @@ export default async (request, context) => {
   const lang = decodeURIComponent(new URL(request['url']).searchParams.get('lang'))
   const nav = decodeURIComponent(new URL(request['url']).searchParams.get('nav'))
   const ig = new URL(request['url']).searchParams.get('ig')
-  const ids = getStore('ids')
   const headers = {}
   if (lang) headers['Accept-Language'] = lang;
   if (nav) headers['User-Agent'] = nav;
@@ -16,20 +15,17 @@ export default async (request, context) => {
 
   if (ig) return;
   if (ns) {
-    const { stores } = await listStores();
-    console.log('### STORES')
-    console.log(stores)
-    console.log('### STORES')
     const id = crypto.createHash('md5').update(`${context['ip']}${context['geo']['city']}`).digest('hex');
-    if (!ids.get(id)) await ids.set(id, id)
+    const ids = getStore('ids')
+    const userId = await ids.get(id)
+    if (!userId) await ids.set(id, id)
     const idVal = await ids.get(id)
     url = `https://cdn.usefathom.com/?h=${encodeURIComponent(page)}&sid=CWSVCDJC&cid=${idVal}`
   } else {
     url = `https://cdn.usefathom.com/?h=${encodeURIComponent(page)}&sid=CWSVCDJC&cid=${num}`
   }
 
-  fetch(`https://cdn.usefathom.com/?h=${encodeURIComponent(page)}&sid=CWSVCDJC&cid=${num}`,
-    { headers })
+  fetch(url, { headers })
     .then((data) => {
       console.log(data)
       return {}
