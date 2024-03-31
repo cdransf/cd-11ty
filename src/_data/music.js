95d003e5a71da952915b3a1ca11cc28df6e52d32
Cory Dransfeldt <coryd@hey.com>
2024-03-30 20:26:38 -0700

    chore: update music api

--- src/_data/music.js
+++ src/_data/music.js
@@ -1,13 +1,17 @@
-// import EleventyFetch from '@11ty/eleventy-fetch';
-// import { buildChart } from './helpers/music.js'
+import EleventyFetch from '@11ty/eleventy-fetch';
+import { buildChart } from './helpers/music.js'
 
-// export default async function () {
-//   const API_KEY_MUSIC = process.env.API_KEY_MUSIC;
-//   const url = `https://coryd.dev/api/music?key=${API_KEY_MUSIC}`;
-//   const res = EleventyFetch(url, {
-//     duration: '1h',
-//     type: 'json',
-//   }).catch();
-//   const resObj = await res;
-//   return buildChart(resObj['data'])
-// }
+export default async function () {
+  const API_KEY_MUSIC = process.env.API_KEY_MUSIC;
+  const url = `https://coryd.dev/api/music?key=${API_KEY_MUSIC}`;
+  const res = EleventyFetch(url, {
+    duration: '1h',
+    type: 'json',
+  }).catch();
+  const resObj = await res;
+  return {
+    charts: buildChart(resObj['scrobbles']),
+    artists: resObj['artists'],
+    albums: resObj['albums']
+  }
+}
