1a6d840a4ab203e43a53426b28b7e9afd8a8dacb
Cory Dransfeldt <coryd@hey.com>
2024-04-02 11:26:18 -0700

    chore: debug

--- api/debug.js
+++ api/debug.js
@@ -1,6 +1,10 @@
 export default async (context, request) => {
   console.log(context)
   console.log(request)
+  const data = await request.formData()
+  console.log(data)
+  const payload = JSON.parse(data.get('payload'))
+  console.log(payload)
 
   return new Response(JSON.stringify({
       status: 'success',
