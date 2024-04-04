eb0aea4627eb66f595f3f0d51de955f5e78b6f5d
Cory Dransfeldt <coryd@hey.com>
2024-04-03 20:17:12 -0700

    chore: debug

--- src/_data/test.js
+++ src/_data/test.js
@@ -1,13 +1,19 @@
 import fs from 'fs'
 
 export default async function () {
-  const window = fs.readFileSync('./json/scrobbles-window.json', (error, data) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    return JSON.parse(data)
-  })
-  console.log(window)
-  return window
+  // const window = fs.readFileSync('./json/scrobbles-window.json', (error, data) => {
+  //   if (error) {
+  //     console.log(error);
+  //     return;
+  //   }
+  //   return JSON.parse(data)
+  // })
+  // console.log(window)
+  // return window
+
+  fs.readdir('./json', (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
 }
