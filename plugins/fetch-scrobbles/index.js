913d9fd8d31ef1b9639be0ed249292a39bc71c27
Cory Dransfeldt <coryd@hey.com>
2024-04-03 19:23:39 -0700

    chore: debug

--- plugins/fetch-scrobbles/index.js
+++ plugins/fetch-scrobbles/index.js
@@ -2,9 +2,27 @@ import { getStore } from '@netlify/blobs'
 import fs from 'fs'
 
 export const onPreBuild = async ({ constants }) => {
-  const scrobbles = getStore('scrobbles')
-  const artists = getStore('artists')
-  const albums = getStore('albums')
+  const scrobbles = getStore({
+    name: 'scrobbles',
+    options: {
+      siteID: constants.SITE_ID,
+      token: constants.NETLIFY_API_TOKEN,
+    }
+  })
+  const artists = getStore({
+    name: 'artists',
+    options: {
+      siteID: constants.SITE_ID,
+      token: constants.NETLIFY_API_TOKEN,
+    }
+  })
+  const albums = getStore({
+    name: 'albums',
+    options: {
+      siteID: constants.SITE_ID,
+      token: constants.NETLIFY_API_TOKEN,
+    }
+  })
   const windowData = await scrobbles.get('window', { type: 'json'})
   const artistsMap = await artists.get('artists-map', { type: 'json' })
   const albumsMap = await albums.get('albums-map', { type: 'json' })
