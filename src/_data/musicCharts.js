149ee7e713107e930d423508229d02599c901e47
Cory Dransfeldt <coryd@hey.com>
2024-04-04 17:20:37 -0700

    chore: pull month chart data

--- src/_data/musicCharts.js
+++ src/_data/musicCharts.js
@@ -5,7 +5,6 @@ export default async function () {
   const monthChart = JSON.parse(await readFile('./src/_data/json/scrobbles-month-chart.json', 'utf8'));
   const artists = JSON.parse(await readFile('./src/_data/json/artists-map.json', 'utf8'));
   const albums = JSON.parse(await readFile('./src/_data/json/albums-map.json', 'utf8'));
-  console.log(buildChart(monthChart['data'], artists, albums))
   return {
     month: buildChart(monthChart['data'], artists, albums)
   }
