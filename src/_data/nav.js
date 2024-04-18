31ba75e8b4be6242994deb18491304bb9685712f
Cory Dransfeldt <coryd@hey.com>
2024-04-17 17:56:31 -0700

    chore: footer links

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -23,6 +23,7 @@ export default async function () {
       { name: 'Mastodon', url: 'https://social.lol/@cory', icon: 'brand-mastodon' },
       { name: 'Last.fm', url: 'https://www.last.fm/user/coryd_', icon: 'brand-lastfm' },
       { name: 'Trakt', url: 'https://trakt.tv/users/cdransf', icon: 'device-tv' },
+      { name: 'Instapaper', url: 'https://www.instapaper.com/p/coryd', icon: 'news' },
       { name: 'Webrings', url: '/webrings', icon: 'heart-handshake' },
     ],
   }
