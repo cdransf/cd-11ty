538f529961154d63f3b41149a26a434ecc6a5d8c
Cory Dransfeldt <coryd@hey.com>
2024-04-28 17:25:07 -0700

    fix: checks

--- plugins/fetch-scrobbles/index.js
+++ plugins/fetch-scrobbles/index.js
@@ -52,24 +52,18 @@ export const onPreBuild = async ({ constants }) => {
   const nowPlaying = await scrobbles.get('now-playing', { type: 'json'})
 
   for (const key of monthKeys) {
-    const scrobbleData = await scrobbles.get(key, { type: 'json'}) || []
-    monthChartData?.['data']?.push(...scrobbleData?.['data'])
+    const scrobbleData = await scrobbles.get(key, { type: 'json'})
+    if (scrobbleData) monthChartData?.['data']?.push(...scrobbleData?.['data'])
   }
 
   for (const key of threeMonthKeys) {
-    const scrobbleData = await scrobbles.get(key, { type: 'json'}) || []
-    threeMonthChartData?.['data']?.push(...scrobbleData?.['data'])
+    const scrobbleData = await scrobbles.get(key, { type: 'json'})
+    if (scrobbleData) threeMonthChartData?.['data']?.push(...scrobbleData?.['data'])
   }
 
   for (const key of yearKeys) {
-    let scrobbleData
-    try {
-      scrobbleData = await scrobbles?.get(key, { type: 'json'})
-    } catch (error) {
-      console.log("Error: can't get more scrobbles.")
-      break;
-    }
-    yearChartData?.['data']?.push(...scrobbleData?.['data'])
+    const scrobbleData = await scrobbles?.get(key, { type: 'json'})
+    if (scrobbleData) yearChartData?.['data']?.push(...scrobbleData?.['data'])
   }
 
   fs.writeFileSync('./src/_data/json/weekly-top-artists-chart.json', JSON.stringify({...weeklyChartData, timestamp: `${lastWeek.set({ hour: 8, minute: 0, second: 0, millisecond: 0 }).toMillis()}` }))
