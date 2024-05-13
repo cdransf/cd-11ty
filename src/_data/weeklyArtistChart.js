b0a29b43196c0c45bdcc5096634a77fd9c59301c
Cory Dransfeldt <hi@coryd.dev>
2024-05-13 14:32:27 -0700

    chore: cleanup

--- src/_data/weeklyArtistChart.js
+++ src/_data/weeklyArtistChart.js
@@ -64,7 +64,6 @@ export default async function() {
       })
     }
 
-    // Fetch the listens data for the past week
     let { data: listens, error } = await supabase
       .from('listens')
       .select(`
