9332b5ab2adc0947d1abc44f6d71624bc18d062f
Cory Dransfeldt <hi@coryd.dev>
2024-05-13 17:48:14 -0700

    fix: guard against null entries

--- config/filters/index.js
+++ config/filters/index.js
@@ -141,7 +141,7 @@ export default {
   // feeds
   normalizeEntries: (entries) => {
     const posts = []
-    if (!entries.length) return posts
+    if (!entries) return posts
     entries.forEach((entry) => {
       const dateKey = Object.keys(entry).find((key) => key.includes('date'))
       const date = new Date(entry[dateKey])
