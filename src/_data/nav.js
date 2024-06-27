c324fae7d7b021cd761ebec45d639397a3042500
Cory Dransfeldt <hi@coryd.dev>
2024-06-27 09:51:57 -0700

    chore: nav

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -6,13 +6,12 @@ export default async function () {
       { name: 'Watching', url: '/watching', icon: 'device-tv' },
       { name: 'Books', url: '/books', icon: 'books' },
       { name: 'Links', icon: 'link' },
-      { name: 'About', url: '/about', icon: 'info-square' },
+      { name: 'About', url: '/about', icon: 'info-circle' },
       { name: 'Search', icon: 'search' },
       { name: 'Feeds', icon: 'rss' },
       { name: 'Mastodon', icon: 'brand-mastodon' },
     ],
     footer: [
-      { name: 'Now' },
       { name: 'Uses' },
       { name: 'Colophon' },
       { name: 'Blogroll' },
@@ -25,6 +24,7 @@ export default async function () {
       { name: 'Mastodon', url: 'https://social.lol/@cory', icon: 'brand-mastodon' },
       { name: 'ListenBrainz', url: 'https://listenbrainz.org/user/cdransf/', icon: 'brain' },
       { name: 'Coffee', url: 'https://buymeacoffee.com/cory', icon: 'coffee' },
+      { name: 'Now', url: '/now', icon: 'clock-hour-3' },
       { name: 'Webrings', url: '/webrings', icon: 'heart-handshake' },
     ],
   }
