6723935876d163bf5aac73572b282fa820d1c611
Cory Dransfeldt <hi@coryd.dev>
2024-08-01 10:20:57 -0700

    chore: formatting consistency

--- workers/analytics/index.js
+++ workers/analytics/index.js
@@ -1,39 +1,34 @@
-const ScriptName = '/js/script.js';
-const Endpoint = '/api/event';
+const scriptName = '/js/script.js'
+const endpoint = '/api/event'
 
 addEventListener('fetch', event => {
-    event.passThroughOnException();
-    event.respondWith(handleRequest(event));
-});
+  event.passThroughOnException()
+  event.respondWith(handleRequest(event))
+})
 
 async function handleRequest(event) {
-    const url = new URL(event.request.url);
-    const pathname = url.pathname;
+  const url = new URL(event.request.url)
+  const pathname = url.pathname
 
-    if (pathname === ScriptName) {
-        return getScript(event);
-    } else if (pathname === Endpoint) {
-        return postData(event);
-    }
-    return new Response(null, { status: 404 });
+  if (pathname === scriptName) getScript(event)
+  if (pathname === endpoint) return postData(event)
+  return new Response(null, { status: 404 })
 }
 
 async function getScript(event) {
-    const cache = caches.default;
-    let response = await cache.match(event.request);
+  const cache = caches.default
+  let response = await cache.match(event.request)
 
-    if (!response) {
-        const scriptUrl = "https://plausible.io/js/plausible.outbound-links.tagged-events.js";
-        response = await fetch(scriptUrl);
-
-        if (response.ok) event.waitUntil(cache.put(event.request, response.clone()));
-    }
-
-    return response;
+  if (!response) {
+    const scriptUrl = 'https://plausible.io/js/plausible.outbound-links.tagged-events.js'
+    response = await fetch(scriptUrl)
+    if (response.ok) event.waitUntil(cache.put(event.request, response.clone()))
+  }
+  return response
 }
 
 async function postData(event) {
-    const request = new Request(event.request);
-    request.headers.delete('cookie');
-    return await fetch("https://plausible.io/api/event", request);
+  const request = new Request(event.request)
+  request.headers.delete('cookie')
+  return await fetch('https://plausible.io/api/event', request)
 }
