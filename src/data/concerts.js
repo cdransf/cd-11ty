cbbd442d64513f25a00059b66be2a0e51244e8a9
Cory Dransfeldt <hi@coryd.dev>
2024-08-01 13:26:55 -0700

    chore: property names

--- src/data/concerts.js
+++ src/data/concerts.js
@@ -46,26 +46,23 @@ const fetchAllConcerts = async () => {
 
 const processConcerts = (concerts) => {
   return concerts.map(concert => ({
-    id: concert['id'],
     date: concert['date'],
-    artist_name_string: concert['artist_name_string'],
+    artistNameString: concert['artist_name_string'],
     venue: {
-      id: concert['venue'],
       name: concert['venue_name'],
       latitude: concert['latitude'],
       longitude: concert['longitude'],
-      bounding_box: concert['bounding_box'],
+      boundingBox: concert['bounding_box'],
       notes: concert['venue_notes']
     },
     notes: concert['concert_notes'],
     artist: concert['artist'] ? {
-      id: concert['artist'],
       name: concert['artist_name'],
       mbid: concert['artist_mbid'],
       country: parseCountryField(concert['artist_country'])
     } : null,
     url: `/concerts/${concert['id']}`,
-    artist_url: concert['artist'] ? `/music/artists/${sanitizeMediaString(concert['artist_name'])}-${sanitizeMediaString(parseCountryField(concert['artist_country']))}` : null
+    artistUrl: concert['artist'] ? `/music/artists/${sanitizeMediaString(concert['artist_name'])}-${sanitizeMediaString(parseCountryField(concert['artist_country']))}` : null
   }))
 }
 
