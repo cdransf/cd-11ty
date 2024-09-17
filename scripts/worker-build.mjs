9f90a264bed4daa4f4fed036de9cde9c6f05486e
Cory Dransfeldt <hi@coryd.dev>
2024-09-17 14:59:20 -0700

    feat: rss to mastodon worker
    	modified:   src/includes/base.liquid

--- scripts/worker-build.mjs
+++ scripts/worker-build.mjs
@@ -13,16 +13,21 @@ if (!workerName) {
 const templatePath = `workers/${workerName}/wrangler.template.toml`
 const outputPath = `workers/${workerName}/wrangler.toml`
 
-try {
-  const template = await fs.readFile(templatePath, 'utf8')
-  const output = template
-    .replace(/\${CF_ACCOUNT_ID}/g, process.env.CF_ACCOUNT_ID)
-    .replace(/\${CF_ZONE_ID}/g, process.env.CF_ZONE_ID)
+async function generateToml() {
+  try {
+    const template = await fs.readFile(templatePath, 'utf8')
+    const output = template
+      .replace(/\${CF_ACCOUNT_ID}/g, process.env.CF_ACCOUNT_ID)
+      .replace(/\${CF_ZONE_ID}/g, process.env.CF_ZONE_ID)
+      .replace(/\${RSS_TO_MASTODON_KV_NAMESPACE_ID}/g, process.env.RSS_TO_MASTODON_KV_NAMESPACE_ID)
 
-  await fs.writeFile(outputPath, output)
+    await fs.writeFile(outputPath, output)
 
-  console.log(`Generated wrangler.toml for ${workerName}`)
-} catch (error) {
-  console.error('Error generating wrangler.toml:', error)
-  process.exit(1)
+    console.log(`Generated wrangler.toml for ${workerName}`)
+  } catch (error) {
+    console.error('Error generating wrangler.toml:', error)
+    process.exit(1)
+  }
 }
+
+generateToml()
---
+++ workers/mastodon/index.js
@@ -0,0 +1,82 @@
+import { XMLParser } from 'fast-xml-parser'
+import { convert } from 'html-to-text'
+
+export default {
+  async scheduled(event, env, ctx) {
+    await handleScheduledEvent(env)
+  },
+
+  async fetch(request, env, ctx) {
+    return Response.redirect('https://coryd.dev/feeds', 301)
+  }
+}
+
+async function handleScheduledEvent(env) {
+  const mastodonApiUrl = 'https://social.lol/api/v1/statuses'
+  const accessToken = env.MASTODON_ACCESS_TOKEN
+  const rssFeedUrl = 'https://coryd.dev/feeds/all'
+
+  try {
+    const latestItems = await fetchRSSFeed(rssFeedUrl)
+
+    for (let i = latestItems.length - 1; i >= 0; i--) {
+      const item = latestItems[i]
+      const existingPost = await env.RSS_TO_MASTODON_NAMESPACE.get(item.link)
+
+      if (existingPost) continue
+
+      const plainTextDescription = convert(item.description, {
+        wordwrap: 150,
+      })
+
+      const content = `${item.title}\n\n${plainTextDescription}\n\n${item.link}`
+      await postToMastodon(mastodonApiUrl, accessToken, content)
+
+      const timestamp = new Date().toISOString()
+      await env.RSS_TO_MASTODON_NAMESPACE.put(item.link, timestamp)
+
+      console.log(`Posted and stored URL: ${item.link}`)
+    }
+
+    console.log('RSS feed processed successfully')
+  } catch (error) {
+    console.error('Error in scheduled event:', error)
+  }
+}
+
+async function fetchRSSFeed(rssFeedUrl) {
+  const response = await fetch(rssFeedUrl)
+  const rssText = await response.text()
+  const parser = new XMLParser()
+  const rssData = parser.parse(rssText)
+  const items = rssData.rss.channel.item
+
+  let latestItems = []
+
+  items.forEach(item => {
+    const title = item.title
+    const link = item.link
+    const description = item.description
+    latestItems.push({ title, link, description })
+  })
+
+  return latestItems
+}
+
+async function postToMastodon(apiUrl, accessToken, content) {
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ status: content })
+  })
+
+  if (!response.ok) {
+    const errorText = await response.text()
+    throw new Error(`Error posting to Mastodon: ${response.statusText} - ${errorText}`)
+  }
+
+  console.log('Posted to Mastodon successfully!')
+}
