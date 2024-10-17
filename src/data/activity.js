bbae08a92569941f07972376b1861a6ec8d353ee
Cory Dransfeldt <hi@coryd.dev>
2024-10-17 16:38:20 -0700

    fix: typo

--- src/data/activity.js
+++ src/data/activity.js
@@ -4,13 +4,13 @@ const SUPABASE_URL = process.env.SUPABASE_URL
 const SUPABASE_KEY = process.env.SUPABASE_KEY
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-export default async function fetchSyndication() {
+export default async function fetchActivity() {
   const { data, error } = await supabase
     .from('optimized_all_activity')
     .select('feed')
 
   if (error) {
-    console.error('Error fetching search index data:', error)
+    console.error('Error fetching activity data:', error)
     return []
   }
 
